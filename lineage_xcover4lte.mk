f47afac5092fdab37737e2f3d208070c53deb920
MrBrause <143131566+MrBrause@users.noreply.github.com>
2023-11-10 12:23:31 +0100

    Update lineage_xcover4lte.mk
    Add build-specifics

--- lineage_xcover4lte.mk
+++ lineage_xcover4lte.mk
@@ -5,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 ## Inherit from xcover4lte device
-$(call inherit-product, device/samsung/xcover4lte/device.mk)
+$(call inherit-product, device/samsung/xcover4ltexx/device.mk)
 
 ## Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -18,9 +18,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_xcover4lte
-PRODUCT_DEVICE := xcover4lte
+PRODUCT_NAME := lineage_xcover4ltexx
+PRODUCT_DEVICE := xcover4ltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G390F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=xcover4ltexx \
+  TARGET_DEVICE=xcover4ltexx \
+  PRIVATE_BUILD_DESC="xcover4ltex-user 9.0 PPR1.180610.011 G390FXXS6CTI2 release-keys"
+
+BUILD_FINGERPRINT := samsung/xcover4ltexx/xcover4lte:9/PPR1.180610.011/G390FXXS6CTI2:user/release-keys
